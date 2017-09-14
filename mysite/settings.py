0a3da6d3ea11b53c3b0098a280ebcc04c823b1eb
kmogu <kmogun@hotmail.com>
2017-09-14 21:39:42 +0200

    añadimos static y primera plantilla real

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    '/home/kmogu/my-first-blog/blog/static',
+    '/home/kmogu/my-first-blog/blog/static/',
 ]
