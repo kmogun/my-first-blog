0d5d410c64f91406b8c8b51349c04ce51a78cf69
kmogu <kmogun@hotmail.com>
2017-09-15 21:33:06 +0200

    pruebas de carga de plantilla

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.baseindex),
+    url(r'^tienda/$', views.plantillabase),
 ]
--- blog/views.py
+++ blog/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def baseindex(request):
     return render(request, 'blog/baseindex.html', {})
+
+def plantillabase(request):
+    return render(request, 'blog/plantilla_base.html', {})
