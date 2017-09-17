e1467d927e420cbfa53d9eb021e8368b204b7673
kmogu <kmogun@hotmail.com>
2017-09-17 18:04:02 +0200

    añadido login

--- blog/views.py
+++ blog/views.py
@@ -11,16 +11,16 @@ from django.urls import reverse_lazy, reverse
 
 # Create your views here.
 
-def baseindex(request):
-    return render(request, 'blog/baseindex.html', {})
+'''def baseindex(request):
+    return render(request, 'blog/baseindex.html', {})'''
 
-def plantillabase(request):
-    return render(request, 'blog/plantilla_base.html', {})
+'''def plantillabase(request):
+    return render(request, 'blog/plantilla_base.html', {})'''
 
 class LoginView(FormView):
     form_class = AuthenticationForm
     template_name = "blog/login.html"
-    success_url = reverse_lazy("blog/baseindex.html")
+    success_url = reverse_lazy("blog/plantilla_base.html")
 
     def dispatch(self, request, *args, **kwargs):
         if request.user.is_authenticated():
@@ -48,7 +48,7 @@ class LoginRequiredMixin(object):
             return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 class ControlPanelView(LoginRequiredMixin, TemplateView):
-    template_name = 'blog/login.html'
+    template_name = 'blog/plantilla_base.html'
 
     def get_context_data(self, **kwargs):
         context = super(ControlPanelView, self).get_context_data(**kwargs)
