4362824b271d2883d6c65f0a7e633c78880f03a4
kmogu <kmogun@hotmail.com>
2017-09-20 00:39:07 +0200

    añadido login

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,7 @@
 from django.conf.urls import include, url
 from . import views
-from .views import ControlPanelView, LoginView, LogoutView
 
 urlpatterns = [
-    #url(r'^$', views.baseindex),
-    #url(r'^tienda/$', views.plantillabase),
-    url(r'^$', ControlPanelView.as_view()),
-    url(r'^login/$', LoginView.as_view()),
-    url(r'^logout/$', LogoutView.as_view()),
+    url(r'^$', views.baseindex),
+    url(r'^tienda/$', views.plantillabase),
 ]
