c031e326e1fa8028a0d35b40948cf6b68edc3e80
kmogun <kmogun@hotmail.com>
2017-09-21 11:01:05 +0200

    pruebas forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 
 class RegistroUsuarios(forms.Form):
-    nombre = forms.CharField(max_length=150)
-    apellido1 = forms.CharField(max_length=150)
-    apellido2 = forms.CharField(max_length=150)
-    email = forms.EmailField()
-    telefono = forms.IntegerField()
+    nombre = forms.CharField(max_length=150, label='Tu nombre:')
+    apellido1 = forms.CharField(max_length=150, label='Tu apellido:')
+    apellido2 = forms.CharField(max_length=150, label='Tu segundo apellido:')
+    email = forms.EmailField(label='Tu e-mail:')
+    telefono = forms.IntegerField(label='Tu telefono:')
 
     def __str__(self):
-        return '{0} {1}'.format(self.nombre, self.email)
+        return '{0}'.format(self.email)
