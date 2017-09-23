d7bb11e0deb652429c450bab7903ceb56ad36bdd
kmogu <kmogun@hotmail.com>
2017-09-23 14:26:36 +0200

    aplicacion presentacion en la web

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def baseindex(request):
-    return render(request, 'blog/prueba.html', {})
+    return render(request, 'blog/baseindex.html', {})
 
 def plantillabase(request):
     return render(request, 'blog/plantilla_base.html', {})
