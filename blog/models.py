d59351e1352dfda44c82465341f94da621d81b36
kmogu <kmogun@hotmail.com>
2017-09-23 14:13:16 +0200

    aplicacion presentacion en la web

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from django.forms import ModelForm
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
 
 # Create your models here.
 
